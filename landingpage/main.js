91d1ec64542b923e52d6573c1a5a94ce7f48079f
Arielle Beltran <arielle.appled@gmail.com>
2024-11-05 21:04:03 -0800

    Updated js & css for the landing page
    With the help of chatgpt made it so that when the hamburger is clicked the contents are placed below it while moving all elements below the dropdown so that they are still visible. I also played around with the font styles and bg colors for the dropwdown content

--- landingpage/main.js
+++ landingpage/main.js
@@ -2,20 +2,51 @@
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("aDropdown").classList.toggle("show");
-  }
+// function myFunction() {
+//     document.getElementById("aDropdown").classList.toggle("show");
+//   }
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
+//   // Close the dropdown menu if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropBttn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+// }
+
+function myFunction() {
+    var dropdown = document.getElementById("aDropdown");
+    var container = document.querySelector(".container");
+    
+    // Toggle the display of the dropdown
+    dropdown.classList.toggle("show");
+    
+    // If dropdown is shown, adjust the margin of the container
+    if (dropdown.classList.contains("show")) {
+        container.style.marginTop = "125px";  // Adjust this value based on the dropdown height
+    } else {
+        container.style.marginTop = "0";  // Reset the margin when dropdown is hidden
+    }
+}
+
+// Close the dropdown if clicked outside
+window.onclick = function(event) {
     if (!event.target.matches('.dropBttn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
+        // Reset the margin when clicking outside
+        document.querySelector(".container").style.marginTop = "0";
     }
 }
