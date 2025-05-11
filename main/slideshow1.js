a9793c9b6cd6f9f48c05d6531ef894bbaa2b0e4d
Arielle Beltran <arielle.appled@gmail.com>
2025-05-10 23:26:25 -0700

    added more slides to graphic design
    + added img

--- main/slideshow1.js
+++ main/slideshow1.js
@@ -58,20 +58,24 @@
 //   dots[slideIndex-1].className += " active";
 // }
 
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
+let slideIndex = [1,1,1]; //tracks # of slides
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0); //initializes slideshow
 showSlides(1, 1);
+showSlides(1, 2);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
+//n checks if user moves forward or backward in the slides
+//no checks which slideshow to modify
+
 function showSlides(n, no) {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
+  let x = document.getElementsByClassName(slideId[no]); //x stores all slide elements belonging to that slideshow
+  if (n > x.length) {slideIndex[no] = 1} //if n is greater than amount of slides, it loops back to start
+  if (n < 1) {slideIndex[no] = x.length} //if n is less than 1 it goes to the last slide
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
