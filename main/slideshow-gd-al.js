7a0fcd2308ba1314d801f65a2bbaa8b57e07078f
Arielle Beltran <arielle.appled@gmail.com>
2025-05-25 02:34:32 -0700

    added an image

--- main/slideshow-gd-al.js
+++ main/slideshow-gd-al.js
@@ -1,9 +1,10 @@
-let slideIndex = [1,1,1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
+let slideIndex = [1,1,1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
+showSlides(1, 4);
 
 
 function plusSlides(n, no) {
